3d8b9ce15c432160da3a2b77c811f217d37afffa
jixiaojie@tom.com <jixiaojie@tom.com>
2018-11-30 02:20:05 -0800

    first commit

--- ros/devel/share/styx_msgs/cmake/styx_msgs-msg-extras.cmake
+++
@@ -1,2 +0,0 @@
-set(styx_msgs_MESSAGE_FILES "/jixj/uda/term3/c25/p006/ros/src/styx_msgs/msg/TrafficLight.msg;/jixj/uda/term3/c25/p006/ros/src/styx_msgs/msg/TrafficLightArray.msg;/jixj/uda/term3/c25/p006/ros/src/styx_msgs/msg/Waypoint.msg;/jixj/uda/term3/c25/p006/ros/src/styx_msgs/msg/Lane.msg")
-set(styx_msgs_SERVICE_FILES "")
